d936e365e94d78c6ecbf544eecbe246c246cc50a
ALongkorn Petchkhaw <ALongkorn Petchkhaw>
2016-04-26 18:45:46 +0700

    #01 สร้างหน้าต่างเกมขึ้นมา

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Galaxy Wars");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	
+	}
+}
