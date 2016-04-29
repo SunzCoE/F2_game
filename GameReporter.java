940e38aec2d7d29dff26afce2a4ceb39217a95a7
ALongkorn Petchkhaw <ALongkorn Petchkhaw>
2016-04-29 22:13:25 +0700

    Revert "#02 เพิ่มส่วนของการเก็บคะแนน"
    This reverts commit 9539f89994c548daa224b3673c41baceb93733d3.

--- GameReporter.java
+++
@@ -1,7 +0,0 @@
-package OOP.F2_game;
-
-public interface GameReporter {
-
-	long getScore();
-
-}
--- Main2.java
+++ Main2.java
@@ -1,5 +1,3 @@
-package OOP.F2_game;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -14,14 +12,5 @@ public class Main2 {
 		JPanel bg = new JPanel();
 			bg.setBackground(Color.LIGHT_GRAY);
 				frame.add(bg);
-				
-		GamePanel gp = new GamePanel();
-		GameEngine engine = new GameEngine(gp, v);
-		frame.addKeyListener(engine);
-		frame.getContentPane().add(gp, BorderLayout.CENTER);
-		frame.setVisible(true);
-		
-		engine.start();
-		}		
-	
+		}
 }
